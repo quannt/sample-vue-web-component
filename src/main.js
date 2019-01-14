c5978b81a265477ee613784015559977ef457c86
Quan <simpleplan.vn@gmail.com>
2019-01-15 00:16:55 +0700

    Add POC for custom widget

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import wrap from '@vue/web-component-wrapper'
+import HelloWorld from './components/HelloWorld'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+
+
+const CustomElement = wrap(Vue, HelloWorld)
+
+window.customElements.define('idf-widget', CustomElement)
